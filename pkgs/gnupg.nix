143787042c2416da333ce40d0f6116a69b54b9a9
Jasper Woudenberg <mail@jasperwoudenberg.com>
2018-03-17 17:05:33 +0100

    Use pinentry-curses with gnupg
    The necessary program was installed but not used.

--- pkgs/gnupg.nix
+++ pkgs/gnupg.nix
@@ -4,8 +4,12 @@ let
   pinentry = pkgs.pinentry.override {
     ncurses = pkgs.ncurses;
   };
+  gnupg = pkgs.gnupg.overrideAttrs(oldAttrs: rec {
+    pinentryBinaryPath = "bin/pinentry-curses";
+    configureFlags = "--with-pinentry-pgm=${pinentry}/${pinentryBinaryPath}";
+  });
 in
-pkgs.gnupg.override {
+gnupg.override {
   libusb = pkgs.libusb;
   pinentry = pinentry;
 }

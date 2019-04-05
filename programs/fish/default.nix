323e4c8e09fc9f637cd518177a06c04bdaabdba9
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-04-05 21:59:09 +0100

    FZF integration in Fish

--- programs/fish/default.nix
+++ programs/fish/default.nix
@@ -1,10 +1,14 @@
 { pkgs, ... }:
 let
-  fish_functions = builtins.attrNames (builtins.readDir ./functions);
-  fish_function_files = builtins.map (fn: ./functions + "/${fn}") fish_functions;
-  macos_init_files = if pkgs.stdenv.isDarwin then [./macos.fish] else [];
-  fish_init_files = [./config.fish] ++ macos_init_files ++ fish_function_files;
-  shellInit = builtins.foldl' (xs: x: "${xs}\n${builtins.readFile x}") "" fish_init_files;
+  fishFunctions = builtins.attrNames (builtins.readDir ./functions);
+  fishFunctionFiles = builtins.map (fn: ./functions + "/${fn}") fishFunctions;
+  macosInitFiles = if pkgs.stdenv.isDarwin then [./macos.fish] else [];
+  fish_init_files = [./config.fish] ++ macosInitFiles ++ fishFunctionFiles;
+  shellInit = ''
+    ${builtins.foldl' (xs: x: "${xs}\n${builtins.readFile x}") "" fish_init_files}
+    source ${pkgs.fzf}/share/fish/vendor_functions.d/fzf_key_bindings.fish
+    source ${pkgs.fzf}/share/fish/vendor_conf.d/load-fzf-key-bindings.fish
+  '';
 in
 {
   programs.fish = {

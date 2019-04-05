6cd8f56ebf19270b65fbe87308bf7f07b38040ea
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-04-05 08:12:26 +0100

    Correctly set $NIX_PATH in fish shell on MacOS

--- programs/fish/default.nix
+++ programs/fish/default.nix
@@ -2,7 +2,8 @@
 let
   fish_functions = builtins.attrNames (builtins.readDir ./functions);
   fish_function_files = builtins.map (fn: ./functions + "/${fn}") fish_functions;
-  fish_init_files = [./config.fish] ++ fish_function_files;
+  macos_init_files = if pkgs.stdenv.isDarwin then [./macos.fish] else [];
+  fish_init_files = [./config.fish] ++ macos_init_files ++ fish_function_files;
   shellInit = builtins.foldl' (xs: x: "${xs}\n${builtins.readFile x}") "" fish_init_files;
 in
 {

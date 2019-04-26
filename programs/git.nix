364f012e86ab51f9759da675aba66c5ddb8a2f3d
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-04-26 12:58:43 +0100

    Add files git should ignore by default

--- programs/git.nix
+++ programs/git.nix
@@ -7,6 +7,11 @@
 
     lfs.enable = true;
 
+    ignores = [
+      "todo.txt"
+      "lorri.log"
+    ];
+
     extraConfig = {
       core.pager = "${pkgs.gitAndTools.diff-so-fancy}/bin/diff-so-fancy | less --tabs=4 -RFX";
     };

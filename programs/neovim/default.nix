38f72d04f7daea0d56d1ea90f272afa6a8818af8
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-09-03 08:02:53 +0100

    Update neovim configuration
    Move configurations from the deprecated `configuration` key to
    replacement keys.

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 let
-  plugins = pkgs.vimPlugins // pkgs.callPackage ./custom-plugins.nix {};
+  allPlugins = pkgs.vimPlugins // pkgs.callPackage ./custom-plugins.nix {};
 in
 {
   programs.neovim = {
@@ -13,39 +13,37 @@ in
 
     vimAlias = true;
 
-    configure = {
-      customRC = builtins.readFile ./vimrc;
+    extraConfig = builtins.readFile ./vimrc;
 
-      packages.plugins.start = with plugins; [
-        ale
-        candid
-        fzfWrapper
-        fzf-vim
-        gitgutter
-        goyo
-        gv
-        lightline-vim
-        neoformat
-        polyglot
-        quickfix-reflector
-        tabnine
-        todo
-        unimpaired
-        vim-abolish
-        vim-commentary
-        vim-eunuch
-        vim-fugitive
-        vim-highlightedyank
-        vim-localvimrc
-        vim-repeat
-        vim-rhubarb
-        vim-signature
-        vim-speeddating
-        vim-surround
-        vim-test
-        vim-vinegar
-        visual-star-search
-      ];
-    };
+    plugins = with allPlugins; [
+      ale
+      candid
+      fzfWrapper
+      fzf-vim
+      gitgutter
+      goyo
+      gv
+      lightline-vim
+      neoformat
+      polyglot
+      quickfix-reflector
+      tabnine
+      todo
+      unimpaired
+      vim-abolish
+      vim-commentary
+      vim-eunuch
+      vim-fugitive
+      vim-highlightedyank
+      vim-localvimrc
+      vim-repeat
+      vim-rhubarb
+      vim-signature
+      vim-speeddating
+      vim-surround
+      vim-test
+      vim-vinegar
+      visual-star-search
+    ];
   };
 }

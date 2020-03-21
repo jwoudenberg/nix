96b7d06b33701cfb3a6f70c17163bcd9ae7d2d50
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-03-21 20:34:10 +0000

    Upgrade random-colors
    New version brings support for non-iterm

--- dev/nix/overlays/random-colors.nix
+++ dev/nix/overlays/random-colors.nix
@@ -4,7 +4,7 @@ let
   src = super.fetchFromGitHub {
     owner = "jwoudenberg";
     repo = "random-colors";
-    rev = "a32eb5f030749012994045bd85dd354cd28c2fd0";
-    sha256 = "0qak191nw5bicks3xp54vyfnx5is3g7i8vnwda36nffa8qa34r21";
+    rev = "c53c6c31bb6868e5dc2ce3cc284d1064146aeece";
+    sha256 = "0aq5pc7qslfm6fz30ziczrz8n0cj2qawcdrdwjv4vblwk634q7x7";
   };
 in { random-colors = super.callPackage src { }; }

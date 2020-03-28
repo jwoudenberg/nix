a82432a155125bdeb2e9b2be025280fd7919ff0b
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-03-28 22:33:43 +0000

    Upgrade random-colors

--- dev/nix/overlays/random-colors.nix
+++ dev/nix/overlays/random-colors.nix
@@ -4,7 +4,7 @@ let
   src = super.fetchFromGitHub {
     owner = "jwoudenberg";
     repo = "random-colors";
-    rev = "c53c6c31bb6868e5dc2ce3cc284d1064146aeece";
-    sha256 = "0aq5pc7qslfm6fz30ziczrz8n0cj2qawcdrdwjv4vblwk634q7x7";
+    rev = "f6a74bc77042a33b3d14a4b56eabefcac024ff8e";
+    sha256 = "0d7616xwr30alx8shdw834dmffbg3sky1rd3mb2nq809skif46vp";
   };
 in { random-colors = super.callPackage src { }; }

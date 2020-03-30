2201b5e9e88d944b5e9b9d0a2ee31b248231448a
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-03-30 20:28:32 +0100

    Format sway configuration file

--- dev/nix/programs/sway.nix
+++ dev/nix/programs/sway.nix
@@ -14,6 +14,7 @@
     focus_on_window_activation smart
     mouse_warping output
     workspace_layout default
+    workspace 1
 
     client.focused #4c7899 #285577 #ffffff #2e9ef4 #285577
     client.focused_inactive #333333 #5f676a #ffffff #484e50 #5f676a
@@ -22,59 +23,59 @@
     client.placeholder #000000 #0c0c0c #ffffff #000000 #0c0c0c
     client.background #ffffff
 
-    bindsym Mod4+0 workspace 10
-    bindsym Mod4+1 workspace 1
-    bindsym Mod4+2 workspace 2
-    bindsym Mod4+3 workspace 3
-    bindsym Mod4+4 workspace 4
-    bindsym Mod4+5 workspace 5
-    bindsym Mod4+6 workspace 6
-    bindsym Mod4+7 workspace 7
-    bindsym Mod4+8 workspace 8
-    bindsym Mod4+9 workspace 9
-    bindsym Mod4+Down focus down
-    bindsym Mod4+Left focus left
-    bindsym Mod4+Return exec alacritty -e fish
-    bindsym Mod4+Right focus right
-    bindsym Mod4+Shift+0 move container to workspace 10
-    bindsym Mod4+Shift+1 move container to workspace 1
-    bindsym Mod4+Shift+2 move container to workspace 2
-    bindsym Mod4+Shift+3 move container to workspace 3
-    bindsym Mod4+Shift+4 move container to workspace 4
-    bindsym Mod4+Shift+5 move container to workspace 5
-    bindsym Mod4+Shift+6 move container to workspace 6
-    bindsym Mod4+Shift+7 move container to workspace 7
-    bindsym Mod4+Shift+8 move container to workspace 8
-    bindsym Mod4+Shift+9 move container to workspace 9
-    bindsym Mod4+Shift+Down move down
-    bindsym Mod4+Shift+Left move left
-    bindsym Mod4+Shift+Right move right
-    bindsym Mod4+Shift+Up move up
-    bindsym Mod4+Shift+h move left
-    bindsym Mod4+Shift+j move down
-    bindsym Mod4+Shift+k move up
-    bindsym Mod4+Shift+l move right
-    bindsym Mod4+Shift+q kill
-    bindsym Mod4+Up focus up
-    bindsym Mod4+f fullscreen toggle
-    bindsym Mod4+h focus left
-    bindsym Mod4+j focus down
-    bindsym Mod4+k focus up
-    bindsym Mod4+l focus right
-    bindsym Mod4+p exec rofi -show run
-
-    mode "resize" {
-    bindsym Down resize grow height 10 px or 10 ppt
-    bindsym Escape mode default
-    bindsym Left resize shrink width 10 px or 10 ppt
-    bindsym Return mode default
-    bindsym Right resize grow width 10 px or 10 ppt
-    bindsym Up resize shrink height 10 px or 10 ppt
+    bindsym {
+      Mod4+0 workspace 10
+      Mod4+1 workspace 1
+      Mod4+2 workspace 2
+      Mod4+3 workspace 3
+      Mod4+4 workspace 4
+      Mod4+5 workspace 5
+      Mod4+6 workspace 6
+      Mod4+7 workspace 7
+      Mod4+8 workspace 8
+      Mod4+9 workspace 9
+      Mod4+Down focus down
+      Mod4+Left focus left
+      Mod4+Return exec alacritty -e fish
+      Mod4+Right focus right
+      Mod4+Shift+0 move container to workspace 10
+      Mod4+Shift+1 move container to workspace 1
+      Mod4+Shift+2 move container to workspace 2
+      Mod4+Shift+3 move container to workspace 3
+      Mod4+Shift+4 move container to workspace 4
+      Mod4+Shift+5 move container to workspace 5
+      Mod4+Shift+6 move container to workspace 6
+      Mod4+Shift+7 move container to workspace 7
+      Mod4+Shift+8 move container to workspace 8
+      Mod4+Shift+9 move container to workspace 9
+      Mod4+Shift+Down move down
+      Mod4+Shift+Left move left
+      Mod4+Shift+Right move right
+      Mod4+Shift+Up move up
+      Mod4+Shift+h move left
+      Mod4+Shift+j move down
+      Mod4+Shift+k move up
+      Mod4+Shift+l move right
+      Mod4+Shift+q kill
+      Mod4+Up focus up
+      Mod4+f fullscreen toggle
+      Mod4+h focus left
+      Mod4+j focus down
+      Mod4+k focus up
+      Mod4+l focus right
+      Mod4+p exec rofi -show run
     }
 
+    mode "resize" bindsym {
+      Down resize grow height 10 px or 10 ppt
+      Escape mode default
+      Left resize shrink width 10 px or 10 ppt
+      Return mode default
+      Right resize grow width 10 px or 10 ppt
+      Up resize shrink height 10 px or 10 ppt
+    }
 
     bar {
-
       font pango:FiraCode 12
       mode hide
       hidden_state hide
@@ -83,6 +84,7 @@
       workspace_buttons yes
       strip_workspace_numbers no
       tray_output primary
+
       colors {
         background #000000
         statusline #ffffff
@@ -93,7 +95,6 @@
         urgent_workspace #2f343a #900000 #ffffff
         binding_mode #2f343a #900000 #ffffff
       }
-
     }
 
     output DVI-D-1 {
@@ -105,7 +106,5 @@
       xkb_variant ,nodeadkeys
       xkb_options ctrl:nocaps
     }
-
-    workspace 1
   '';
 }

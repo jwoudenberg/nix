4e50fc0d32f6e0969fbb8b63b825f8abaffb4d95
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-04-04 12:02:52 +0100

    Re-disable tests for pass
    I forgot why I did this, then found out when trying to update my Nix
    configuration on MacOS: pass tests don't run there (at least not with my
    configurations enabled).

--- dev/nix/overlays/pass.nix
+++ dev/nix/overlays/pass.nix
@@ -4,4 +4,9 @@ let
     configureFlags =
       "--with-pinentry-pgm=${super.pinentry}/bin/pinentry-curses";
   });
-in { pass = super.pass.override { gnupg = gnupg; }; }
+in {
+  pass = (super.pass.override { gnupg = gnupg; }).overrideAttrs (oldAttrs: {
+    # The install check fails on MacOS.
+    doInstallCheck = false;
+  });
+}

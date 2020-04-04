9aac8cc68e31ba4375e15f1eec7a735df4f3fb6d
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-04-04 17:34:35 +0100

    Initial nix-darwin configuration
    For the moment I'm not doing much with this yet, except run home-manager
    through it.

---
+++ dev/nix/darwin-configuration.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ pkgs.cowsay
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  environment.darwinConfig = "$HOME/dev/nix/darwin-configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.bash.enable = true;
+  # programs.zsh.enable = true;
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # You should generally set this to the total number of logical cores in your system.
+  # $ sysctl -n hw.ncpu
+  nix.maxJobs = 8;
+  nix.buildCores = 8;
+
+  imports = [ <home-manager/nix-darwin> ];
+
+  users.users.jasper = {
+    home = "/Users/jasper";
+  };
+
+  home-manager.users.jasper = import ./home-macos.nix;
+}

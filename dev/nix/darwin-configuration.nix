3c9db73a3186b6750ae7745e05ea7da224d00073
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-04-04 18:09:04 +0100

    Fix fish PATH ordering issue

--- dev/nix/darwin-configuration.nix
+++ dev/nix/darwin-configuration.nix
@@ -16,8 +16,21 @@
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.bash.enable = true;
   programs.fish.enable = true;
-  # programs.zsh.enable = true;
-  # programs.fish.enable = true;
+  # Hack to address an issue with Fish. We should be able to remove this once
+  # fish has been upgraded to version 3.1.0 or later (looks like this will be in
+  # the 20.09 nixpkgs release at the earliest). Addresses this problem:
+  # https://github.com/LnL7/nix-darwin/issues/122
+  #
+  # How to verify removing this doesn't break anything: check the $PATH to see
+  # the nix profile is loaded first. `which vim` should load a Nix-provided Vim,
+  # not a MacOS provided one.
+  programs.fish.shellInit = ''
+    for p in /Users/jasper/.nix-profile/bin
+      if not contains $p $fish_user_paths
+        set -g fish_user_paths $p $fish_user_paths
+      end
+    end
+  '';
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog

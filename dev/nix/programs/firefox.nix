ce228f9e22d54db094daf73244e9bf97882af80b
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-04-04 23:22:18 +0100

    Add some firefox configuration

--- dev/nix/programs/firefox.nix
+++ dev/nix/programs/firefox.nix
@@ -2,5 +2,19 @@
   programs.firefox = {
     enable = true;
     package = pkgs.firefox-wayland;
+    profiles.jasper = {
+      id = 0;
+      isDefault = true;
+      settings = {
+        "browser.startup.homepage" = "about:blank";
+        "browser.urlbar.suggest.searches" = false;
+        "browser.urlbar.suggest.history" = false;
+        "network.cookie.cookieBehavior" = 1;
+        "privacy.clearOnShutdown.cookies" = true;
+        "browser.newtabpage.enabled" = false;
+        "browser.urlbar.oneOffSearches" = false;
+        "browser.search.defaultenginename" = "bing";
+      };
+    };
   };
 }

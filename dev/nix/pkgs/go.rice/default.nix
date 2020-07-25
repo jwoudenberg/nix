c23f6c5124deae8581c6e433728716b0f888f8c4
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-07-25 17:05:40 +0100

    Provide hash for go.rice sources

--- dev/nix/pkgs/go.rice/default.nix
+++ dev/nix/pkgs/go.rice/default.nix
@@ -1,11 +1,13 @@
-{ buildGoPackage, stdenv }:
+{ fetchFromGitHub, buildGoPackage, stdenv }:
 
 let
   ref = "d954009f7238df62c766980934c8ea7f161d0e59";
 
-  src = builtins.fetchGit {
-    url = "git@github.com:GeertJohan/go.rice";
-    ref = ref;
+  src = fetchFromGitHub {
+    owner = "GeertJohan";
+    repo = "go.rice";
+    rev = ref;
+    sha256 = "19lca5hcw9milgmjv6vlcg8vq4mz9a9sp5bpihxvyza0da6pklmf";
   };
 
 in buildGoPackage {
@@ -16,7 +18,7 @@ in buildGoPackage {
   goDeps = ./deps.nix;
   meta = with stdenv.lib; {
     description =
-      "go.rice is a Go package that makes working with resources such as html,js,css,images,templates, etc very easy.";
+      "A Go package that makes working with resources such as html,js,css,images,templates, etc very easy.";
     homepage = "https://github.com/GeertJohan/go.rice";
     license = licenses.bsd2;
     maintainers = [ ];

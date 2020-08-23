e539880eefa2c85a65230be261e59e215913deb5
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-08-23 10:56:22 +0200

    Grab similar-sort from its dedicated repo

--- dev/nix/overlays/similar-sort.nix
+++ dev/nix/overlays/similar-sort.nix
@@ -1,12 +1,4 @@
 self: super:
 
-{
-  similar-sort = let
-    brianDotfiles = super.fetchFromGitHub {
-      owner = "BrianHicks";
-      repo = "dotfiles.nix";
-      rev = "df928289675cbeb6e72ddc08829604f174efcba6";
-      sha256 = "1vrk56mp206hm4xrl01sgbihlzlyzgi7w291hzinqikmx4qrmp2i";
-    };
-    in super.callPackage (brianDotfiles + "/pkgs/similar-sort") { };
-}
+let sources = import ../nix/sources.nix;
+in { similar-sort = self.callPackage sources.similar-sort { }; }

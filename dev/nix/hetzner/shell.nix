59a9484ff8b77f53a65058fb4d19f58cab9f9c34
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-10-25 17:55:17 +0100

    Move keys in pass database

--- dev/nix/hetzner/shell.nix
+++ dev/nix/hetzner/shell.nix
@@ -9,12 +9,12 @@ let
     set -euo pipefail
 
     # For terraform
-    export HCLOUD_TOKEN="$(pass show keys/hetzner_cloud_token)"
+    export HCLOUD_TOKEN="$(pass show hetzner.com | grep 'API-token' | awk '{print $2}')"
     export CLOUDFLARE_EMAIL="$(pass show cloudflare.com | grep email | awk '{print $2}')"
-    export CLOUDFLARE_API_TOKEN="$(pass show keys/cloudflare_api_token)"
+    export CLOUDFLARE_API_TOKEN="$(pass show cloudflare.com | grep 'API-token' | awk '{print $2}')"
 
     # For nix deploys
-    export RESILIO_MUSIC_READONLY="$(pass show keys/resilio_music_readonly)"
+    export RESILIO_MUSIC_READONLY="$(pass show resilio.com | grep 'key-music-read-only' | awk '{print $2}')"
     exec "$@"
   '';
 

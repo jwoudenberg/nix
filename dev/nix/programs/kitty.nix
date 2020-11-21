42fb855ad3a7643ad46905f06e4bd505343f5918
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-11-21 17:52:56 +0100

    Keyboard shortcut and other kitty configuration

--- dev/nix/programs/kitty.nix
+++ dev/nix/programs/kitty.nix
@@ -10,6 +10,15 @@
     settings = {
       font_size = 18;
       shell = "${pkgs.fish}/bin/fish";
+      tab_bar_edge = "top";
+      tab_bar_style = "separator";
+      tab_separator = " | ";
+    };
+
+    keybindings = {
+      "ctrl+c" = "copy_or_interrupt";
+      "ctrl+v" = "paste_from_clipboard";
+      "ctrl+k" = "clear_terminal clear active";
     };
   };
 }

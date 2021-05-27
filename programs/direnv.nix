87c4cd5dc5b537f81702dea92e69b4c89003b841
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-05-27 22:32:30 +0200

    Don't use custom direnv stdlib
    At second though, I don't like this pattern much. Whenever collaborating
    on a repo it assumes folks all have relevant custom stdlib functions in
    their personal direnv configurations.

--- programs/direnv.nix
+++ programs/direnv.nix
@@ -2,12 +2,5 @@
   programs.direnv = {
     enable = true;
     enableNixDirenvIntegration = true;
-    stdlib = ''
-      use_flake() {
-        watch_file flake.nix
-        watch_file flake.lock
-        eval "$(nix print-dev-env)"
-      }
-    '';
   };
 }

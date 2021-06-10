89fdf5426c7e67c4f4b7064ca3f16c8d442c2de8
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-06-10 23:18:34 +0200

    More firefox configuration

--- programs/firefox.nix
+++ programs/firefox.nix
@@ -1,17 +1,29 @@
 { pkgs, ... }: {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-wayland;
-    profiles.jasper = {
-      id = 0;
-      isDefault = true;
-      settings = {
-        "browser.startup.homepage" = "about:blank";
-        "browser.urlbar.suggest.searches" = false;
-        "browser.urlbar.suggest.history" = false;
-        "network.cookie.cookieBehavior" = 1;
-        "browser.newtabpage.enabled" = false;
-        "signon.rememberSignons" = false;
+    package = pkgs.firefox-wayland.override {
+      extraPrefs = ''
+        pref("browser.download.dir", "~/downloads");
+        pref("browser.download.useDownloadDir", true);
+        pref("browser.link.open_newwindow", 0);
+        pref("browser.newtabpage.enabled", false);
+        pref("browser.startup.homepage", "about:blank");
+        pref("browser.tabs.allowTabDetach", false);
+        pref("browser.urlbar.suggest.bookmark", false);
+        pref("browser.urlbar.suggest.engines", false);
+        pref("browser.urlbar.suggest.history", false);
+        pref("browser.urlbar.suggest.quicksuggest", false);
+        pref("browser.urlbar.suggest.searches", false);
+        pref("browser.urlbar.suggest.topsites", false);
+        pref("browser.warnOnQuit", false);
+        pref("extensions.pocket.enabled", false);
+        pref("identity.fxaccounts.enabled", false);
+        pref("network.cookie.cookieBehavior", 1);
+        pref("signon.rememberSignons", false);
+      '';
+      extraPolicies = {
+        SearchEngines = { Default = "DuckDuckGo"; };
+        ShowHomeButton = false;
       };
     };
   };

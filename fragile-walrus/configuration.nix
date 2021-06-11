74a43b5bee7dc1b1ab7be6633942b7805cb302e0
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-06-11 21:27:34 +0200

    Store gnupg across reboots

--- fragile-walrus/configuration.nix
+++ fragile-walrus/configuration.nix
@@ -43,6 +43,7 @@ inputs:
     ln -sfn /persist/random-colors /home/jasper/.config/random-colors
     ln -sfn /persist/gh /home/jasper/.config/gh
     ln -sfn /persist/awsvault /home/jasper/.awsvault
+    ln -sfn /persist/gnupg /home/jasper/.gnupg
 
     # Tailscale state
     mkdir -p /var/lib/tailscale

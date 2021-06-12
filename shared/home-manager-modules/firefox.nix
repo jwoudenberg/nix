4354174ae34cb34031010e0ddc2597ffa1223986
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-06-12 10:16:15 +0200

    More Firefox first-run tweaks

--- shared/home-manager-modules/firefox.nix
+++ shared/home-manager-modules/firefox.nix
@@ -3,14 +3,14 @@
     enable = true;
     package = pkgs.firefox-wayland.override {
       extraPrefs = ''
+        pref("browser.aboutwelcome.enabled", false);
         pref("browser.download.dir", "~/downloads");
         pref("browser.download.useDownloadDir", true);
         pref("browser.link.open_newwindow", 3);
         pref("browser.link.open_newwindow.restriction", 0);
         pref("browser.newtabpage.enabled", false);
+        pref("browser.startup.firstrunSkipsHomepage", false);
         pref("browser.startup.homepage", "about:blank");
-        pref("browser.aboutwelcome.enabled", false);
-        pref("trailhead.firstrun.branches", "nofirstrun-empty");
         pref("browser.tabs.allowTabDetach", false);
         pref("browser.urlbar.suggest.bookmark", false);
         pref("browser.urlbar.suggest.engines", false);
@@ -23,6 +23,7 @@
         pref("identity.fxaccounts.enabled", false);
         pref("network.cookie.cookieBehavior", 1);
         pref("signon.rememberSignons", false);
+        pref("trailhead.firstrun.branches", "nofirstrun-empty");
       '';
       extraPolicies = {
         SearchEngines = { Default = "DuckDuckGo"; };

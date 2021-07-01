bce1fcd6943549fae33c37a811b70b52fb1e09f3
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-07-01 20:45:45 +0200

    Add back ai-banana to ssh config
    I still want to set the default user.

--- shared/home-manager-modules/ssh.nix
+++ shared/home-manager-modules/ssh.nix
@@ -2,6 +2,7 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
+      "ai-banana" = { user = "root"; };
       "rsyncnet" = {
         hostname = "ch-s012.rsync.net";
         user = "19438";

87885640b0e68e83fb5222aacb35b82fefc17761
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-07-01 20:19:36 +0200

    Remove ai-banana from ssh config
    Tailscale and magic DNS mean I can get in without needing to go over the
    open internet.

--- shared/home-manager-modules/ssh.nix
+++ shared/home-manager-modules/ssh.nix
@@ -2,10 +2,6 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
-      "ai-banana" = {
-        hostname = "ai-banana.jasperwoudenberg.com";
-        user = "root";
-      };
       "rsyncnet" = {
         hostname = "ch-s012.rsync.net";
         user = "19438";

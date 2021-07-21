c27374962d25403470c141081a1c579fada94782
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-07-21 08:49:52 +0200

    Switch back to stable ZFS

--- fragile-walrus/configuration.nix
+++ fragile-walrus/configuration.nix
@@ -21,8 +21,6 @@ inputs:
   boot.kernelPackages =
     pkgs.linuxPackages_5_12; # Required by AMD 6700 graphics card
   boot.loader.systemd-boot.enable = true;
-  boot.zfs.enableUnstable =
-    true; # Required until ZFS for kernel 5.12 is officially released.
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 10;
   boot.kernel.sysctl."fs.inotify.max_user_watches" = 524288;

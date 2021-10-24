9af8f79d820c74d57e7f1539e91191bc209328bd
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-10-24 11:52:39 +0200

    Fix linux setup

--- fragile-walrus/configuration.nix
+++ fragile-walrus/configuration.nix
@@ -111,8 +111,8 @@ inputs:
   nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = [ inputs.self.overlays.linuxCustomPkgs ];
 
-  nix.registry.nixpkgs.flake = inputs.nixpkgs;
-  nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+  nix.registry.nixpkgs.flake = inputs.nixpkgs-nixos;
+  nix.nixPath = [ "nixpkgs=${inputs.nixpkgs-nixos}" ];
   nix.binaryCaches = [ "https://cache.nixos.org" "https://nri.cachix.org" ];
   nix.binaryCachePublicKeys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="

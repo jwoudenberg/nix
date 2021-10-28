b2eec13905ff814ee721d3ecc4885fce19eb2c19
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-10-28 16:36:48 +0200

    Pull in yubikey-based ssh keys

--- ai-banana/configuration.nix
+++ ai-banana/configuration.nix
@@ -68,7 +68,7 @@ in inputs:
   users.users.root.openssh.authorizedKeys.keys = let
     sshKeysSrc = builtins.fetchurl {
       url = "https://github.com/jwoudenberg.keys";
-      sha256 = "sha256:1kix1w98bj85rnwpd00y6qck3mrghli7m9fyir6sv6ld8s4kpq54";
+      sha256 = "sha256:1wfnzni5hw4q1jiki058l4qxxfzdbrp51d7l47bsq1wb20a7284i";
     };
   in builtins.filter (line: line != "")
   (pkgs.lib.splitString "\n" (builtins.readFile sshKeysSrc));

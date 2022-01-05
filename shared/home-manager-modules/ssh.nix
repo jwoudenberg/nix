334fe8e21813c8eebe7f8c9506ef6e2cb67998f2
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-01-05 11:56:36 +0100

    Enable ssh encryption

--- shared/home-manager-modules/ssh.nix
+++ shared/home-manager-modules/ssh.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   programs.ssh = {
     enable = true;
+    compression = true; # Needed by tmate
     matchBlocks = {
       "ai-banana" = { user = "root"; };
       "rsyncnet" = {

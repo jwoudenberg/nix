b6bdfa2e545a8416153cecd798713d16af31b887
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-04-14 16:00:08 +0200

    fragile-walrus: remove rsyncnet configuration
    I'm no longer using this service.

--- shared/home-manager-modules/ssh.nix
+++ shared/home-manager-modules/ssh.nix
@@ -2,12 +2,6 @@
   programs.ssh = {
     enable = true;
     compression = true; # Needed by tmate
-    matchBlocks = {
-      "ai-banana" = { user = "root"; };
-      "rsyncnet" = {
-        hostname = "ch-s012.rsync.net";
-        user = "19438";
-      };
-    };
+    matchBlocks = { "ai-banana" = { user = "root"; }; };
   };
 }

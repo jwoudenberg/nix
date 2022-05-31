d2374e26b61241944b52560e152df7c36ec351fc
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-05-31 15:48:00 +0200

    neovim: bring back hotkeys for git hunks
    The gitsigns plugin used to include these, but default keybindings were
    disabled in a recent update.

--- shared/home-manager-modules/neovim/init.lua
+++ shared/home-manager-modules/neovim/init.lua
@@ -313,9 +313,38 @@ vim.cmd([[
 ]])
 
 -- GIT
-require('gitsigns').setup({current_line_blame = true})
-require('plenary') -- Needs to be loaded before neogit: https://github.com/TimUntersberger/neogit/issues/206
-require('neogit').setup({})
+require('gitsigns').setup({
+    current_line_blame = true,
+
+    on_attach = function(bufnr)
+        -- Adapted from the gitsings README example:
+        -- https://github.com/lewis6991/gitsigns.nvim
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+        end, {expr = true})
+
+        map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+        end, {expr = true})
+
+        -- Actions
+        map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+        map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    end
+})
 
 -- SUBSTITUTIONS
 vim.api.nvim_set_keymap("n", "s", [[<plug>(SubversiveSubstitute)]], {})

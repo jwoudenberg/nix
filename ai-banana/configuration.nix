70093cb6fe327c959e1334a6e8ef79a27a83f831
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-10-08 22:19:14 +0200

    mbsync: perform a health check after syncing

--- ai-banana/configuration.nix
+++ ai-banana/configuration.nix
@@ -456,6 +456,8 @@ in inputs:
       ${pkgs.isync}/bin/mbsync \
         --config ${mbsyncConfigFile} \
         --all
+      ${pkgs.curl}/bin/curl --retry 3 \
+        https://hc-ping.com/f4f0e24f-9d45-4191-96b6-914759ef4bb2/$?
     '';
   };
 

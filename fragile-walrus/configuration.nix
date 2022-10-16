9adcb92f819391ad3f025e9d536013c253b8778c
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-10-16 18:11:43 +0200

    Extract zfs config into a nixos submodule

--- fragile-walrus/configuration.nix
+++ fragile-walrus/configuration.nix
@@ -11,6 +11,7 @@
     ../shared/nixos-modules/sway.nix
     ../shared/nixos-modules/systemd-boot.nix
     ../shared/nixos-modules/yubikey.nix
+    ../shared/nixos-modules/zfs.nix
     ./hardware-configuration.nix
     flakeInputs.home-manager.nixosModules.home-manager
   ];
@@ -25,56 +26,10 @@
   boot.kernel.sysctl."fs.inotify.max_user_watches" = 524288;
 
   # Reset root filesystem at boot
-  boot.initrd.supportedFilesystems = [ "zfs" ];
   boot.initrd.postDeviceCommands = lib.mkAfter ''
     zfs rollback -r trunk/encrypted/root@blank
   '';
-
-  systemd.services.persist-linking = {
-    description = "Create symbolic links to /persist";
-    after = [ "persist.mount" ];
-    wantedBy = [ "multi-user.target" "tailscaled.service" "resilio.service" ];
-    serviceConfig.Type = "oneshot";
-    script = ''
-      set -euxo pipefail
-
-      # Make /persist public so the rslsync user can look in it.
-      chmod 755 /persist
-
-      # Symbolic links to home directory.
-      ln -sfn /persist/rslsync/jasper /home/jasper/docs
-      ln -sfn /persist/rslsync/hjgames /home/jasper/hjgames
-      ln -sfn /persist/dev /home/jasper/dev
-
-      mkdir -p /home/jasper/.config
-      chown jasper:users /home/jasper/.config
-      ln -sfn /persist/random-colors /home/jasper/.config/random-colors
-      ln -sfn /persist/signal /home/jasper/.config/Signal
-      ln -sfn /persist/pijul /home/jasper/.config/pijul
-
-      mkdir -p /home/jasper/.cache
-      chown jasper:users /home/jasper/.cache
-      ln -sfn /persist/nix-index /home/jasper/.cache/nix-index
-
-      # Tailscale state
-      mkdir -p /var/lib/tailscale
-      ln -sfn /persist/tailscale/files /var/lib/tailscale/files
-      ln -sfn /persist/tailscale/tailscaled.state /var/lib/tailscale/tailscaled.state
-      ln -sfn /persist/tailscale/tailscaled.log.conf /var/lib/tailscale/tailscaled.log.conf
-
-      # Set permissions for rslsync dirs (these gets reset sometimes, don't know why)
-      chown -R rslsync:rslsync /persist/rslsync
-      chmod -R 770 /persist/rslsync
-    '';
-  };
-
-  # ZFS
-  boot.supportedFilesystems = [ "zfs" ];
   networking.hostId = "f1e5b37a";
-  services.zfs = {
-    autoScrub.enable = true;
-    trim.enable = true;
-  };
 
   services.keybase.enable = true;
 

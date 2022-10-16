1d8f27bb8dc2d16a7d882d488f5a9cfed59300d0
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-10-16 12:38:52 +0200

    kitty: less neovim UI when using it as a pager

--- shared/home-manager-modules/aerc.nix
+++ shared/home-manager-modules/aerc.nix
@@ -34,7 +34,7 @@
     dirlist-format = %n
 
     [viewer]
-    pager = nvim -R -c 'set ft=mail laststatus=0 nomod nolist'
+    pager = nvim -R -c 'set ft=mail laststatus=0 nomod nolist nonumber'
 
     [compose]
     reply-to-self = false

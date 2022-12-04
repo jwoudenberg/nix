74b6d1081a5acbd209b008e225ede5f95a953f90
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-12-04 13:14:14 +0100

    networking: use bind mounts for tailscale and iwd
    If I create a symlink from /var/lib/iwd to /persist/iwd, that doesn't
    seem to be respected by iwd or tailscale. Could also be a systemd thing.
    So instead I've been linking individual files inside the /persist/iwd
    and /persist/tailscale directories into their respective /var/lib/...
    directories, but that's brittle:
    - New files created by iwd/tailscale aren't automatically persisted
    - Some knowledge is required to know which files to link
    - iwd and tailscale might not accept the symbolic links
    
    I recently learned about this Linux feature called 'bind mounts'. This
    allows a particular directory to be 'mounted' onto another place of the
    filesystem, in a way iwd/tailsscale/systemd can't seem to detect. All
    those programs work now. Also, tailscale now starts immediately on boot
    without requiring me to run `tailscale up`.

--- shared/nixos-modules/networking.nix
+++ shared/nixos-modules/networking.nix
@@ -16,24 +16,16 @@
     [ "systemd-networkd-wait-online.service" ];
 
   systemd.services.networking-persist-linking = {
-    description = "Create symbolic links to /persist";
+    description = "Mount networking dirs from /persist";
     after = [ "persist.mount" ];
     wantedBy = [ "iwd.service" "tailscaled.service" ];
     serviceConfig.Type = "oneshot";
     script = ''
       set -euxo pipefail
 
-
-      # Iwd wifi passwords
-      mkdir -p /persist/iwd
-      for file in /persist/iwd/*.psk; do
-        ln -sfn "$file" "/var/lib/iwd/$(basename "$file")"
-      done
-
-      # Tailscale state
-      mkdir -p /var/lib/tailscale
-      ln -sfn /persist/tailscale/files /var/lib/tailscale/files
-      ln -sfn /persist/tailscale/tailscaled.log.conf /var/lib/tailscale/tailscaled.log.conf
+      mkdir -p /persist/iwd /var/lib/tailscale
+      ${pkgs.mount}/bin/mount --bind /persist/iwd /var/lib/iwd
+      ${pkgs.mount}/bin/mount --bind /persist/tailscale /var/lib/tailscale
     '';
   };
 }

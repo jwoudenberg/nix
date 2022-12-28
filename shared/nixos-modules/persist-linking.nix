4c09ed05b0f97fbc0130036a813a1fea86143521
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-12-28 14:36:37 +0100

    Use bind-mounts for /persist instead of symlinks
    As I'm learning more about Unix/Linux and permissions, I've come to
    realize it would be better if the /persist directory could only be read
    by the root user, so would be owned by root and have 700 permissions.
    That way normal services will not be able to read everything in
    /persist, which they currently can.
    
    Linux users don't have access to a file/directory, unless they are able
    read every parent directory in the path. So making /persist only
    readable by root prevents any other user from looking at files anywhere
    under /persist. Symlinks to it will also only be followable by root.
    
    Using bind mounts it becomes possible to mount part of the /persist
    directory somewhere else on the file system, where other parts users
    might read it depending on the permissions wherever we mount. Systemd
    uses this to provide a measure of isolation for services.

--- shared/nixos-modules/persist-linking.nix
+++ shared/nixos-modules/persist-linking.nix
@@ -1,6 +1,55 @@
 { pkgs, ... }: {
-  systemd.services.persist-linking = {
-    description = "Create symbolic links to /persist";
+  fileSystems."/home/jasper/docs" = {
+    device = "/persist/rslsync/jasper";
+    fsType = "none";
+    options = [ "bind" ];
+    depends = [ "/persist" ];
+  };
+
+  fileSystems."/home/jasper/hjgames" = {
+    device = "/persist/rslsync/hjgames";
+    fsType = "none";
+    options = [ "bind" ];
+    depends = [ "/persist" ];
+  };
+
+  fileSystems."/home/jasper/dev" = {
+    device = "/persist/dev";
+    fsType = "none";
+    options = [ "bind" ];
+    depends = [ "/persist" ];
+  };
+
+  fileSystems."/home/jasper/.config/random-colors" = {
+    device = "/persist/random-colors";
+    fsType = "none";
+    options = [ "bind" ];
+    depends = [ "/persist" ];
+  };
+
+  fileSystems."/home/jasper/.config/Signal" = {
+    device = "/persist/signal";
+    fsType = "none";
+    options = [ "bind" ];
+    depends = [ "/persist" ];
+  };
+
+  fileSystems."/home/jasper/.config/pijul" = {
+    device = "/persist/pijul";
+    fsType = "none";
+    options = [ "bind" ];
+    depends = [ "/persist" ];
+  };
+
+  fileSystems."/home/jasper/.cache/nix-index" = {
+    device = "/persist/nix-index";
+    fsType = "none";
+    options = [ "bind" ];
+    depends = [ "/persist" ];
+  };
+
+  systemd.services.persist-permissions = {
+    description = "Set correct permissions for bind mounts to /persist";
     wantedBy = [ "multi-user.target" "resilio.service" ];
     serviceConfig.Type = "oneshot";
     script = ''
@@ -9,20 +58,9 @@
       # Make /persist public so the rslsync user can look in it.
       chmod 755 /persist
 
-      # Symbolic links to home directory.
-      ln -sfn /persist/rslsync/jasper /home/jasper/docs
-      ln -sfn /persist/rslsync/hjgames /home/jasper/hjgames
-      ln -sfn /persist/dev /home/jasper/dev
-
-      mkdir -p /home/jasper/.config
-      chown jasper:users /home/jasper/.config
-      ln -sfn /persist/random-colors /home/jasper/.config/random-colors
-      ln -sfn /persist/signal /home/jasper/.config/Signal
-      ln -sfn /persist/pijul /home/jasper/.config/pijul
-
-      mkdir -p /home/jasper/.cache
-      chown jasper:users /home/jasper/.cache
-      ln -sfn /persist/nix-index /home/jasper/.cache/nix-index
+      # chown directories in home we're bind-mounting into
+      mkdir -p /home/jasper/{.config,.cache}
+      chown jasper:users /home/jasper/{.config,.cache}
 
       # Set permissions for rslsync dirs (these gets reset sometimes, don't know why)
       chown -R rslsync:rslsync /persist/rslsync

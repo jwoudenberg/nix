f92e8be4f994a13a5f882784ff41d7b2480a493a
Jasper Woudenberg <mail@jasperwoudenberg.com>
2023-01-26 08:27:21 +0100

    Add airborne-cactus to known hosts

--- shared/home-manager-modules/ssh.nix
+++ shared/home-manager-modules/ssh.nix
@@ -11,6 +11,7 @@
         text = ''
           github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
           ai-banana ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJtiovfQ9l6ewuahB3Zh1AnJ8+NNABHwwrqmKtVHXiQ
+          airborne-cactus ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOFIqsk0NjAYwiv8CG3v7YrUTd3mD1iExuyZ85eJed0l
         '';
       };
     in "~/.ssh/known_hosts ${knownHosts}";

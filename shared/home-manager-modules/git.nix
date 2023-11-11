cd7558f6f133fe1f014ef580b9c7d54dd747cf2e
Jasper Woudenberg <mail@jasperwoudenberg.com>
2023-11-11 18:55:53 +0100

    Switch shell colorscheme on branch change
    random-colors does this correctly for bash and fish, but nushell that
    I'm running now didn't have a good API to trigger on git branch changes.
    I recently realized I could use a git hook instead, and now it works
    again!

--- shared/home-manager-modules/git.nix
+++ shared/home-manager-modules/git.nix
@@ -8,6 +8,12 @@
 
     ignores = [ "todo.txt" ".direnv" ".lvimrc" "Session.vim" ];
 
+    hooks.post-checkout =
+      let
+        script = pkgs.writeScriptBin "post-checkout" "${pkgs.random-colors}/bin/random-colors &";
+      in
+      "${script}/bin/post-checkout";
+
     extraConfig = {
       core.pager = "${pkgs.gitAndTools.delta}/bin/delta";
       branch.sort = "-committerdate";

b231d4c99ac768cd63cfd4c6a1659ef795119d69
Jasper Woudenberg <mail@jasperwoudenberg.com>
2023-12-31 14:05:07 +0100

    Add script for generating totp codes from uris in password store
    Use like this:
    
        pass show some-password | totp
    
    This assumes one of the lines in the password file is an oath://.. uri.

--- shared/home-manager-modules/age/default.nix
+++ shared/home-manager-modules/age/default.nix
@@ -7,6 +7,16 @@
         export PASSAGE_RECIPIENTS_FILE="${./recipients}"
         exec ${pkgs.passage}/bin/passage "$@"
       '';
+
+      totp = pkgs.writers.writePython3Bin "totp" { libraries = [ pkgs.python3Packages.pyotp ]; } ''
+        import pyotp
+        import sys
+        for line in sys.stdin:
+            if line.startswith("otpauth"):
+                totp = pyotp.parse_uri(line.strip())
+                print(totp.now())
+                break
+      '';
     in
-    [ pkgs.age pkgs.age-plugin-yubikey wrappedPassage ];
+    [ pkgs.age pkgs.age-plugin-yubikey wrappedPassage totp ];
 }

a0f07fbe81fdc49f9b9eb0198582d6d8007c19d9
Jasper Woudenberg <mail@jasperwoudenberg.com>
2024-01-06 23:36:23 +0100

    Fix public ssh keys

--- shared/nixos-modules/users.nix
+++ shared/nixos-modules/users.nix
@@ -8,7 +8,7 @@
       "$y$j9T$lhBW.HPXfrhWgqf6V5yoi/$j5O92DHx/DNk4TjwXP4N1SDOzdt1n0T8ALeRdJki2QD";
     openssh.authorizedKeys.keys = [
       # Yubikey 5 NFC
-      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCpYScAJfuGpIjYWS1Xf02y5jVFmXD3fsZ0rv364N2uHEKpt7YHaiWX+vCOaNf3j6pW9CPp7jtf/+udwtcXrcb4="
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPm2jdW8FNbI0lwVWbJBJXU8Ib1GVZRhY6Sy10hZFlSobt2RChtPBAUV/WNsG+Cb7jjtNnrHZWKJnO9mvClfg6c="
       # Yubikey 5 USB-C
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLudn9N7mU+ZJTKxgpcPilXGCP1tfoAqg/gy0uT/ZiDiuaz8c83wJqAPBmWKAHGcFDcRbaQLmnnYaYkHHJ9L6xU="
     ];

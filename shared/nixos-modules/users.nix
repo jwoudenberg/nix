80ab0dcfac4a6b413e0517c5f74cc64025b630c8
Jasper Woudenberg <mail@jasperwoudenberg.com>
2024-06-16 15:48:17 +0200

    ssh: Use new sk-ed25519 keys

--- shared/nixos-modules/users.nix
+++ shared/nixos-modules/users.nix
@@ -14,8 +14,10 @@
     openssh.authorizedKeys.keys = [
       # Yubikey 5 NFC
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPm2jdW8FNbI0lwVWbJBJXU8Ib1GVZRhY6Sy10hZFlSobt2RChtPBAUV/WNsG+Cb7jjtNnrHZWKJnO9mvClfg6c="
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICX1KxUJWwIscXsNGnb778Q/nhbg8ir8K0iXZFDsQEzkAAAADnNzaDpZdWJpa2V5U1NI"
       # Yubikey 5 USB-C
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLudn9N7mU+ZJTKxgpcPilXGCP1tfoAqg/gy0uT/ZiDiuaz8c83wJqAPBmWKAHGcFDcRbaQLmnnYaYkHHJ9L6xU="
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINiA5SYUntSswrkNAdP6t5uiIEI/rIgjAuUMmKVluAcnAAAABHNzaDo="
     ];
     uid = 1000;
   };

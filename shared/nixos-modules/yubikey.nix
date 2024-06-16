1a9cd913c2ec0808b96a6fe67e04b03cda43ff66
Jasper Woudenberg <mail@jasperwoudenberg.com>
2024-06-16 12:35:36 +0200

    ssh: Switch from yubikey-agent to using FIDO2 key
    I've liked yubikey-agent, but there's a couple of advantages to using a
    FIDO2 key:
    
    - Can use an ed25519, which is better trusted than ECDSA.
    - I should be able to use the above key for signing git commits
      (avoiding the need to set u gpg keys).

--- shared/nixos-modules/yubikey.nix
+++ shared/nixos-modules/yubikey.nix
@@ -1,10 +1,5 @@
 { pkgs, ... }:
 {
   services.pcscd.enable = true;
-  services.yubikey-agent.enable = true;
-
-  # Use yubikey-agent for ssh access. Need to use a graphical pinentry option
-  # here instead of curses, because pinentry is called by the yubikey-agent
-  # daemon and so doesn't have a connection with the active terminal.
-  programs.gnupg.agent.pinentryPackage = pkgs.pinentry-qt;
+  programs.gnupg.agent.pinentryPackage = pkgs.pinentry-ncurses;
 }

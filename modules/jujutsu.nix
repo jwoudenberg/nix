f154b7ff724ca3aa86fb3f79675b8fcd4bcc7500
Jasper Woudenberg <mail@jasperwoudenberg.com>
2024-10-26 16:53:28 +0200

    jujutsu: add initial configuration file

--- modules/jujutsu.nix
+++ modules/jujutsu.nix
@@ -1,4 +1,12 @@
 { pkgs, config, ... }:
 {
   environment.systemPackages = [ pkgs.jujutsu ];
+
+  homedir.files = {
+    ".config/jj/config.toml" = pkgs.writeText "config.toml" ''
+      [user]
+      name = "Jasper Woudenberg"
+      email = "mail@jasperwoudenberg.com"
+    '';
+  };
 }

7fad543d070574ebed5e690eb461685d728e97c2
Jasper Woudenberg <mail@jasperwoudenberg.com>
2024-12-14 22:49:44 +0100

    pijul: remove persistence helper
    I'm not usinig pijul anymore.

--- modules/persist-linking.nix
+++ modules/persist-linking.nix
@@ -21,13 +21,6 @@
     depends = [ "/persist" "/home/jasper" ];
   };
 
-  fileSystems."/home/jasper/.config/pijul" = {
-    device = "/persist/pijul";
-    fsType = "none";
-    options = [ "bind" ];
-    depends = [ "/persist" "/home/jasper" ];
-  };
-
   fileSystems."/home/jasper/.cache/nix-index" = {
     device = "/persist/nix-index";
     fsType = "none";
